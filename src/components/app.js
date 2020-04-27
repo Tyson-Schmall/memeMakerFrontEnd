5d32ec3e6800bdc67fa1cc5453cfacdb2ecd5f18
Tyson-Schmall <tyson.schmall@gmail.com>
2020-04-27 11:29:43 -0600

    Initial commit (Spun up React skeleton, implemented hookroutes, and meme, memeform docs as well as updated app.js doc, removed index doc entirely, added to bootstrap for the hookrouter.)

---
+++ src/components/app.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import Meme from "./meme";
+
+export default function App() {
+  const [memes, setMemes] = useState([]);
+
+  const deleteMeme = (id) => {
+    axios
+      .delete(`http://localhost:5000/delete_meme/${id}`)
+      .then(setMemes(memes.filter((meme) => meme.id !== id)))
+      .catch((err) => console.log("Error with Deletion : ", err));
+  };
+
+  const renderMemes = () => {
+    return memes.map((meme) => {
+      return <Meme key={meme.id} meme={meme} deleteMeme={deleteMeme} />;
+    });
+  };
+
+  const getMemes = () => {
+    fetch("http://localhost:5000/memes")
+      .then((res) => res.json())
+      .then((data) => setMemes(data))
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getMemes();
+    0;
+  }, []);
+
+  return <div className="app">{renderMemes()}</div>;
+}
